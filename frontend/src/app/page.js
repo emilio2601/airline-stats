ef5ef304f350c1c76ec3a86559eda2aba9849185
Emilio Mendoza <emilio2601@gmail.com>
2025-04-07 19:27:08 -0400

    multi airline filter

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -229,12 +229,22 @@ const CountryFilter = ({ closePopover, setBreakdown, setFilters, setConfig }) =>
 }
 
 const AirlineFilter = ({ closePopover, setBreakdown, setFilters, setConfig }) => {
-  const [airline, setAirline] = useState('')
+  const [airlines, setAirlines] = useState([])
+  const [inputAirline, setInputAirline] = useState("")
 
   const applyFilter = () => {
-    closePopover()
-    setBreakdown(airline)
-    setFilters((f) => ({ ...f, carrier: airline}))
+    let newAirlines = [...airlines, inputAirline].filter((a) => a != "")
+    setAirlines(newAirlines)
+    setBreakdown(newAirlines.join(", "))
+    setFilters((f) => ({ ...f, carrier: newAirlines}))
+    setInputAirline("")
+  }
+
+  const removeAirline = (i) => {
+    let newAirlines = airlines.filter((_, j) => j != i)
+    setAirlines(newAirlines)
+    setBreakdown(newAirlines.join(", "))
+    setFilters((f) => ({ ...f, carrier: newAirlines}))
   }
 
   useEffect(() => setConfig({name: "Airline", keys: ["carrier"]}), [])
@@ -242,8 +252,18 @@ const AirlineFilter = ({ closePopover, setBreakdown, setFilters, setConfig }) =>
   return (
     <>
       <span>Filter by Airline</span>
-      <input type="text" placeholder="IATA code" value={airline} onChange={(e) => setAirline(e.target.value)} className="border p-2"/>
-      <button className='bg-green-500 p-2 text-white rounded-md' onClick={applyFilter}>Apply</button>
+      <input type="text" placeholder="IATA code" value={inputAirline} onChange={(e) => setInputAirline(e.target.value)} className="border p-2"/>
+      {airlines.length > 0 && <div className="flex flex-row space-x-3">
+        {airlines.map((a, i) => (
+          <div key={i} className="flex flex-row space-x-1">
+            <span>{a}</span>
+            <button className="text-red-500" onClick={() => removeAirline(i)}>
+              <i className="fa fa-times-circle"></i>
+            </button> 
+          </div>
+        ))}
+      </div>}
+      <button className='bg-green-500 p-2 text-white rounded-md' onClick={applyFilter}>Add airline</button>
     </>
   )
 }
