d74b358c74c96bd5a020d479f73d953b1c3ba6bc
Emilio Mendoza <emilio2601@gmail.com>
2025-08-06 18:08:02 -0400

    fallback so saved search works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :routes, only: [:index] do
@@ -11,4 +10,11 @@ Rails.application.routes.draw do
   end
 
   resources :saved_searches, only: [:create, :show, :index, :destroy]
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get '*path', to: 'application#fallback_index_html', constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
