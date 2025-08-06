41d38672ecb3c05a5334fe7806ff00f6227da3b3
Emilio Mendoza <emilio2601@gmail.com>
2025-08-06 15:30:18 -0400

    run rails update

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,7 @@ require "action_controller/railtie"
 # require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-# require "sprockets/railtie"
 require "rails/test_unit/railtie"
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,6 +23,11 @@ module AirlineStats
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -32,10 +35,5 @@ module AirlineStats
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-
-    config.active_record.async_query_executor = :global_thread_pool
-    config.active_record.global_executor_concurrency = 8
-    config.active_record.schema_format = :sql
   end
 end
