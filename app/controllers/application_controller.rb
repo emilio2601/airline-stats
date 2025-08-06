f9fd1171fbd8554229aa55c0e9fdb80a0bc59932
Emilio Mendoza <emilio2601@gmail.com>
2025-08-06 18:40:07 -0400

    render from rails root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   include Pagy::Backend
 
   def fallback_index_html
-    render file: 'public/index.html'
+    render file: Rails.root.join('public', 'index.html'), layout: false
   end
 end
