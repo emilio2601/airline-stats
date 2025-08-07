ffd101df7e33a56ba24f21ac72a11c6398d38060
Emilio Mendoza <emilio2601@gmail.com>
2025-08-07 16:30:46 -0400

    fix build

--- frontend/src/app/s/[shareable_id]/page.js
+++ frontend/src/app/s/[shareable_id]/page.js
@@ -1,17 +1,23 @@
-import Home from "@/app/page";
+'use client';
+import Home from "../page";
 import axios from "axios";
 
-export default async function SharedSearch({ params }) {
-  const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3210" : "";
-  let savedSearch = null;
+export const dynamic = 'force-dynamic';
 
+export default async function SavedSearchPage({ params }) {
+  const { shareable_id } = params;
+  
+  const baseURL = process.env.NODE_ENV == "development" ? "http://localhost:3210" : ""
+
+  let savedSearch = null;
   try {
-    const response = await axios.get(`${baseURL}/saved_searches/${params.shareable_id}`);
+    const response = await axios.get(`${baseURL}/saved_searches/${shareable_id}`);
     savedSearch = response.data;
   } catch (error) {
-    console.error("Failed to load shared search:", error);
-    // You might want to render an error page here
+    console.error("Error fetching saved search:", error);
   }
 
-  return <Home initialFilters={savedSearch?.params} savedSearch={savedSearch} />;
+  return (
+    <Home initialFilters={savedSearch?.params} savedSearch={savedSearch} />
+  )
 } 
